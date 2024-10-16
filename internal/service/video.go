147bcc9b93f3e70b46dc6d6851c8197aaa954da2
c8763yee <rrrrr4413@gmail.com>
2024-10-16 23:59:46 +0800

    fix: limit max frame range

--- internal/service/video.go
+++ internal/service/video.go
@@ -2,11 +2,14 @@ package service
 
 import (
 	"bytes"
+	"errors"
 
 	"github.com/c8763yee/mygo-backend/internal/utils"
 )
 
 type VideoService struct{}
+
+const MAX_GIF_FRAMES_DIFF = 1000
 
 func NewVideoService() *VideoService {
 	return &VideoService{}
@@ -17,5 +20,15 @@ func (s *VideoService) ExtractFrame(episode string, frameNumber int) (*bytes.Buf
 }
 
 func (s *VideoService) ExtractGIF(episode string, startFrame, endFrame int) (*bytes.Buffer, error) {
+	// raise error if diff between start and end frames is too large (absolutely arbitrary)
+	frameDiff := endFrame - startFrame
+	if frameDiff < 0 {
+		frameDiff = -frameDiff
+	}
+
+	if frameDiff > MAX_GIF_FRAMES_DIFF {
+		return nil, errors.New("frame diff too large")
+	}
+
 	return utils.ExtractGIF(episode, startFrame, endFrame)
 }
