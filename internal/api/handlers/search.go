18632886c079be8b2f3e80b524e84bf12432f3bd
c8763yee <rrrrr4413@gmail.com>
2025-02-24 15:38:25 +0800

    detail search log message

--- internal/api/handlers/search.go
+++ internal/api/handlers/search.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/c8763yee/mygo-backend/internal/models"
@@ -10,6 +11,7 @@ import (
 )
 
 // Search godoc
+//
 //	@Summary		Search for sentences
 //	@Description	Search for sentences based on query and other parameters
 //	@Tags			search
@@ -27,6 +29,7 @@ func Search(db *gorm.DB) gin.HandlerFunc {
 		}
 
 		searchService := service.NewSearchService(db)
+    fmt.Printf("-------------- [Printf]: Querying %s in %s episode %s (Page %d, paged by %d)----------------\n", req.Query, req.VideoName, req.Episode, req.NthPage, req.PagedBy)
 		result, count, err := searchService.SearchByText(req.Query, req.VideoName, req.Episode, req.PagedBy, req.NthPage)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
